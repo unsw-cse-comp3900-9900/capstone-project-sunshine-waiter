15f14cc9698bcbe086445c417ebc3b7cdb51f8e1
JasonZheng <hbxzknight@gmail.com>
2020-03-10 14:33:22 +1100

    registering users

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@
 const express = require('express')
 const connectDb = require('./src/connection')
 const api = require('./src/api')
+const users = require('./routes/users')
 
 // const
 const app = express()
 const PORT = 8000
 
+app.use(express.json())
 app.use('/api', api)
+app.use('/api/users', users)
 
 app.listen(PORT, () => {
   console.log('Listening at ' + PORT)
