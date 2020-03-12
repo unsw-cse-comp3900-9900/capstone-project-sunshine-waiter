57716b28d8839bd6ccf14e205a77028ff9eee629
JasonZheng <hbxzknight@gmail.com>
2020-03-12 13:22:33 +1100

    Authenticating Users

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const connectDb = require('./src/connection')
 const api = require('./src/api')
 const users = require('./routes/users')
+const auth = require('./routes/auth')
 
 // const
 const app = express()
@@ -11,6 +12,7 @@ const PORT = 8000
 app.use(express.json())
 app.use('/api', api)
 app.use('/api/users', users)
+app.use('/api/auth', auth)
 
 app.listen(PORT, () => {
   console.log('Listening at ' + PORT)
