bcc4d5877ebb8dd72c0bf7e73eb97a200293971a
JasonZheng <hbxzknight@gmail.com>
2020-03-12 14:44:51 +1100

    Generating JWT by a secret which is stored as environment-variables.

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 // lib dependency
+const config = require('config')
 const express = require('express')
 const connectDb = require('./src/connection')
 const api = require('./src/api')
@@ -8,6 +9,14 @@ const auth = require('./routes/auth')
 // const
 const app = express()
 const PORT = 8000
+
+// environment variables
+if (!config.get('jwtPrivatKey')) {
+  console.error(
+    'FATAL ERROR: environment variable "jwtPrivatKey" is not defined.'
+  )
+  process.exit(1)
+}
 
 app.use(express.json())
 app.use('/api', api)
