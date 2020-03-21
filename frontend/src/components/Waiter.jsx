83929dfdcb4d8c9525375cfd3bf386e5db6f53ae
Zitong Li <filr0017@gmail.com>
2020-03-21 19:58:34 +1100

    Finished several major functions of waiter page, like SW-49, SW-50, SW-51. Details of these tasks are on Jira.

--- frontend/src/components/Waiter.jsx
+++ frontend/src/components/Waiter.jsx
@@ -1,63 +1,242 @@
 import React from 'react'
 import './Waiter.css'
 
-const Dish = props => {
-  const { name, id, table, time } = props.info
-  return (
-    <div>
-      <div>name: {name}</div>
-      <div>id: {id}</div>
-      <div>table: {table}</div>
-      <div>time: {time}</div>
-    </div>
-  )
+const READY = 'READY'
+const SERVED = 'SERVED'
+const FAILED = 'FAILED'
+
+const fakeDishes = {
+  djieq34q329r: {
+    _id: 'djieq34q329r',
+    name: 'orange',
+    readyTime: 1584510589,
+    tableId: 't27',
+    orderId: 'fdwji1ri23234',
+    state: READY, //canbe SERVED
+    serveTime: null,
+  },
+  ejiri429999w: {
+    _id: 'ejiri429999w',
+    name: 'plum',
+    readyTime: 1584511060,
+    tableId: 't28',
+    orderId: 'fdafr89891ruq',
+    state: READY, //canbe SERVED
+    serveTime: null,
+  },
+  djieq34q379r: {
+    _id: 'djieq34q379r',
+    name: 'apple',
+    readyTime: 1584511154,
+    tableId: 't27',
+    orderId: 'fdwji1ri23234',
+    state: READY, //canbe SERVED
+    serveTime: null,
+  },
 }
 
-class DishesToServe extends React.Component {
+const fakeNewDish = {
+  _id: 'djieq34q569r',
+  name: 'egg',
+  readyTime: 1584512368,
+  tableId: 't28',
+  orderId: 'fdafr89891ruq',
+  state: READY, //canbe SERVED or FAIL
+  serveTime: null,
+}
+
+const objToArray = obj => {
+  var result = []
+  for (var key in obj) {
+    result.push(obj[key])
+  }
+  return result
+}
+
+const groupBy = (obj, key) => {
+  var array = objToArray(obj)
+  // Return the end result
+  return array.reduce((result, currentValue) => {
+    // If an array already present for key, push it to the array. Else create an array and push the object
+    ;(result[currentValue[key]] = result[currentValue[key]] || []).push(
+      currentValue
+    )
+    // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
+    return result
+  }, {}) // empty object is the initial value for result object
+}
+
+class Dishes extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      dishQue: [
-        {
-          name: 'hamberger',
-          id: 1,
-          table: 2,
-          time: '18:55',
-        },
-        {
-          name: 'egg',
-          id: 2,
-          table: 4,
-          time: '16:55',
-        },
-      ],
+      dishList: fakeDishes,
+      // dishQue: groupBy(fakeDishes, 'tableId'), // Currently use a fake dish list to test
+      finished: {},
+      failed: {},
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-      finished: [],
+  handleClick(dish_id, action, e) {
+    var newDishList = { ...this.state.dishList }
+    var newFinished = { ...this.state.finished }
+    var newFailed = { ...this.state.failed }
+    var targetDish = { ...newDishList[dish_id] }
+    delete newDishList[dish_id]
+    switch (action) {
+      case 'finish':
+        targetDish.state = SERVED
+        targetDish.serveTime = new Date().toLocaleDateString()
+        newFinished[dish_id] = targetDish
+        console.log('dish finished!!!!')
+        console.log(newFinished)
+
+        // TODO: send finished dish to server
+
+        this.setState({
+          dishList: newDishList,
+          finished: newFinished,
+        })
+        break
+      case 'failed':
+        targetDish.state = FAILED
+        targetDish.serveTime = new Date().toLocaleDateString()
+        newFailed[dish_id] = targetDish
+
+        // TODO: send failed dish to server
+
+        this.setState({
+          dishList: newDishList,
+          finished: newFailed,
+        })
+        break
+      case 'reset':
+        targetDish = newFinished[dish_id] || newFailed[dish_id]
+        targetDish.state = READY
+        targetDish.serveTime = null
+        newDishList[dish_id] = targetDish
+        delete newFinished[dish_id]
+
+        // TODO: send reset to server
+
+        this.setState({
+          dishList: newDishList,
+          finished: newFinished,
+        })
+      default:
+        return
     }
   }
 
-  renderDishes() {
-    return this.state.dishQue.map(dish => {
-      return (
-        <tr key={dish._id}>
-          <td>{dish.name}</td>
-          <td>{dish.table}</td>
-          <td>{dish.time}</td>
-          <td>
-            <button>Done</button>
-          </td>
-        </tr>
-      )
-    })
-  }
-
   render() {
-    const dishQue = this.state.dishQue
     return (
       <div className="box">
         <h2>Dishes</h2>
+        <RenderDishes
+          dishList={this.state.dishList}
+          handleClick={this.handleClick}
+        />
+
+        {Object.keys(this.state.finished).length > 0 && <h2>Finished</h2>}
+
+        <RenderFinished
+          dishList={this.state.finished}
+          handleClick={this.handleClick}
+        />
       </div>
     )
+  }
+}
+
+class RenderDishes extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  renderSingleDish(dish) {
+    const renderedDish = (
+      <div className="dishBox" key={dish._id}>
+        <div>{dish.name}</div>
+        <div>{dish.readyTime}</div>
+        <div>
+          <button onClick={e => this.props.handleClick(dish._id, 'finish', e)}>
+            finish
+          </button>
+          <button onClick={e => this.props.handleClick(dish._id, 'failed', e)}>
+            failed
+          </button>
+        </div>
+      </div>
+    )
+    return <div>{dish.state == READY && renderedDish}</div>
+  }
+
+  render() {
+    const dishList = this.props.dishList
+    const dishListGroupedByTableId = groupBy(dishList, 'tableId')
+    var result = []
+    for (var tableId in dishListGroupedByTableId) {
+      const dishes = dishListGroupedByTableId[tableId].map(dish =>
+        this.renderSingleDish(dish)
+      )
+      const tableBox = (
+        <div className="tableBox" key={tableId}>
+          <h3>Table: {tableId}</h3>
+          {dishes}
+        </div>
+      )
+      result.push(tableBox)
+    }
+    return <div>{result}</div>
+  }
+}
+
+class RenderFinished extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  renderSingleDish(dish) {
+    return (
+      <div className="dishBox" key={dish._id}>
+        <div>{dish.name}</div>
+        <div>{dish.readyTime}</div>
+        <div>
+          <button onClick={e => this.props.handleClick(dish._id, 'reset', e)}>
+            reset
+          </button>
+        </div>
+        <div>
+          {dish.state == SERVED && (
+            <h2>
+              <span role="img" aria-label="tick">
+                ☑️
+              </span>
+            </h2>
+          )}
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    const dishList = this.props.dishList
+    const dishListGroupedByTableId = groupBy(dishList, 'tableId')
+    var result = []
+    for (var tableId in dishListGroupedByTableId) {
+      const dishes = dishListGroupedByTableId[tableId].map(dish =>
+        this.renderSingleDish(dish)
+      )
+      const tableBox = (
+        <div className="tableBox" key={tableId}>
+          <h3>Table: {tableId}</h3>
+          {dishes}
+        </div>
+      )
+      result.push(tableBox)
+    }
+    return <div>{result}</div>
   }
 }
 
@@ -73,20 +252,6 @@ class Request extends React.Component {
       ],
       finished: [],
     }
-  }
-
-  renderRequests() {
-    return this.state.requestQue.map(request => {
-      return (
-        <tr key={request._id}>
-          <td>{request.table}</td>
-          <td>{request.time}</td>
-          <td>
-            <button>Done</button>
-          </td>
-        </tr>
-      )
-    })
   }
 
   render() {
@@ -105,7 +270,7 @@ const Waiter = () => (
     </header>
 
     <div id="box-container">
-      <DishesToServe />
+      <Dishes />
       <Request />
     </div>
   </div>
