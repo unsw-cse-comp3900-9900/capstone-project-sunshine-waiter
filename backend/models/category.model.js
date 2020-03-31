90e28c48f206ed19e8f1f1651e94a7d26c74f1ba
JasonZheng <hbxzknight@gmail.com>
2020-03-31 21:09:03 +1100

    Category CRUD v1

---
+++ backend/models/category.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 50,
+  },
+  description: {
+    type: String,
+    required: false,
+    maxlength: 2047,
+  },
+  pic: {
+    type: String,
+    required: false,
+    minlength: 5,
+    maxlength: 200,
+  },
+  menu: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Menu',
+    required: true,
+  },
+})
+
+const Category = mongoose.model('Category', schema)
+
+module.exports = Category
