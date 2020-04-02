f7968ac6c93c68dd2b10d6aac5710b86a50988f9
YAOYELU <luyaoyejeren@gmail.com>
2020-04-02 18:02:18 +1100

    add update and batch get category apis

--- frontend/src/components/apis/actions/category.js
+++ frontend/src/components/apis/actions/category.js
@@ -22,3 +22,45 @@ export const createCategoryItem = (
       .catch(err => console.log({ err }))
   }
 }
+
+export const updateCategoryItem = (
+  token,
+  restaurantId,
+  categoryId,
+  param,
+  callback = () => {}
+) => {
+  if (token !== undefined) {
+    const config = {
+      headers: {
+        'x-auth-token': token,
+        'Content-Type': 'application/json',
+      },
+    }
+    const URL = `/restaurants/${restaurantId}/categories/${categoryId}`
+    BaseProvider.put(URL, param, config)
+      .then(res => {
+        console.log({ res })
+        callback()
+      })
+      .catch(err => console.log({ err }))
+  }
+}
+
+export const getCategoryItem = (token, restaurantId, callback = () => {}) => {
+  if (token !== undefined) {
+    const config = {
+      headers: {
+        'x-auth-token': token,
+        'Content-Type': 'application/json',
+      },
+    }
+    const URL = `/restaurants/${restaurantId}/categories`
+    BaseProvider.get(URL, config)
+      .then(res => {
+        console.log({ res })
+        callback()
+      })
+      .catch(err => console.log({ err }))
+  }
+}
