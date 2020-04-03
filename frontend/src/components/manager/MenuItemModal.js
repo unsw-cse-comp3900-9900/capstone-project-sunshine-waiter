dcbb04df093d88e0c4437a8813c269ee2a0fd1fc
YAOYELU <luyaoyejeren@gmail.com>
2020-04-03 17:15:01 +1100

    add categoryArray session and display it as tags, and finish create menuItem api operation

--- frontend/src/components/manager/MenuItemModal.js
+++ frontend/src/components/manager/MenuItemModal.js
@@ -1,24 +1,136 @@
 import React from 'react'
-import { Modal } from 'antd'
+import { Modal, Tag, Input } from 'antd'
+import { TweenOneGroup } from 'rc-tween-one'
+import _ from 'lodash'
 
 import { createMenuItem } from '../apis/actions/menuItem'
 import { getCookie } from '../authenticate/Cookies'
 
 class MenuItemModal extends React.Component {
   state = {
+    categoryInputVisble: false,
+    categoryInputValue: '',
+    categoryArray: [],
     name: '',
     description: '',
     note: '',
     price: 0,
   }
 
-  onSubmit = () => {
-    createMenuItem(
+  handleCategoryTagDelete = removedTagId => {
+    const categoryArray = this.state.categoryArray.filter(
+      ca => ca !== removedTagId
+    )
+    console.log(categoryArray)
+    this.setState({ categoryArray: categoryArray })
+  }
+
+  showCategoryInput = () => {
+    this.setState({ categoryInputVisble: true })
+  }
+
+  handleCategoryInputValueConfirm = () => {
+    const { categoryInputValue } = this.state
+    let { categoryArray } = this.state
+    if (
+      categoryInputValue &&
+      categoryArray.indexOf(categoryInputValue) === -1
+    ) {
+      categoryArray = [...categoryArray, categoryInputValue]
+    }
+    this.setState({
+      categoryArray,
+      categoryInputVisble: false,
+      categoryInputValue: '',
+    })
+  }
+
+  categoriesTagsMap = tag => {
+    const tagElem = (
+      <Tag
+        closable
+        onClose={e => {
+          e.preventDefault()
+          this.handleCategoryTagDelete(tag)
+        }}
+      >
+        {tag}
+      </Tag>
+    )
+    return (
+      <span key={tag} style={{ display: 'inline-block' }}>
+        {tagElem}
+      </span>
+    )
+  }
+
+  renderCategoryArray = () => {
+    const {
+      categoryInputVisble,
+      categoryInputValue,
+      categoryArray,
+    } = this.state
+    const categoryChild = categoryArray.map(this.categoriesTagsMap)
+    return (
+      <div className="field">
+        <label>CategoryArray</label>
+        <div style={{ marginBottom: 16 }}>
+          <TweenOneGroup
+            enter={{
+              scale: 0.8,
+              opacity: 0,
+              type: 'from',
+              duration: 100,
+              onComplete: e => {
+                e.target.style = ''
+              },
+            }}
+            leave={{ opacity: 0, width: 0, scale: 0, duration: 200 }}
+            appear={false}
+          >
+            {categoryChild}
+          </TweenOneGroup>
+          {categoryInputVisble ? (
+            <Input
+              type="text"
+              size="small"
+              style={{ width: 78 }}
+              value={categoryInputValue}
+              onChange={e =>
+                this.setState({
+                  categoryInputValue: e.target.value,
+                })
+              }
+              onBlur={this.handleCategoryInputValueConfirm}
+              onPressEnter={this.handleCategoryInputValueConfirm}
+            />
+          ) : (
+            <Tag onClick={this.showCategoryInput} className="site-tag-plus">
+              <i className="tag icon" /> add CateTag
+            </Tag>
+          )}
+        </div>
+      </div>
+    )
+  }
+
+  onSubmit = async e => {
+    e.preventDefault()
+    const { onFetchCurrentMenu } = this.props
+    await createMenuItem(
       getCookie('token'),
       this.props.restaurantId,
-      this.state,
-      this.props.onCancel()
+      _.pick(this.state, [
+        'categoryArray',
+        'name',
+        'description',
+        'note',
+        'price',
+      ])
     )
+
+    onFetchCurrentMenu()
+    this.props.onCancel()
   }
 
   render() {
@@ -26,6 +138,7 @@ class MenuItemModal extends React.Component {
     return (
       <Modal visible={visible} onCancel={onCancel} onOk={this.onSubmit}>
         <form className="ui form" onSubmit={this.onSubmit}>
+          {this.renderCategoryArray()}
           <div className="field">
             <label htmlFor="name">Name</label>
             <input
