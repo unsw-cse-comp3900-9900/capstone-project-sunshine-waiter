a9cdeb2e4e99e56b3f37c69bd85d459e067770ea
YAOYELU <luyaoyejeren@gmail.com>
2020-04-08 15:33:32 +1000

    fix backend server crashing by commenting psudo code

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const PORT = 5000
 
-const arrayToObj = (array) => {
+const arrayToObj = array => {
   let result = {}
   for (let item of array) {
     result[item._id] = item
@@ -16,10 +16,10 @@ const nsps = {}
 
 const { dishes, requests } = require('./fakeData')
 
-const onConnection = (anonymousClient) => {
+const onConnection = anonymousClient => {
   console.log('Unknow User ' + anonymousClient.id + ' connected!')
 
-  anonymousClient.on('authenticate', (data) => {
+  anonymousClient.on('authenticate', data => {
     const { restaurantId, _id, type, password } = data
 
     //skip authenticate
@@ -31,24 +31,24 @@ const onConnection = (anonymousClient) => {
       anonymousClient.emit('authenticate success', nspName) // send nsp to authenticated client
       if (!nsps[restaurantId]) {
         const nsp = io.of(nspName) // create safe connect
-        nsp.on('connect', (socket) => {
+        nsp.on('connect', socket => {
           // do sth like first time.
-          listener.on('new order placed', (order) => {
-            nsp.to('cook').emit('update dish queue', queue)
-          })
+          // listener.on('new order placed', (order) => {
+          //   nsp.to('cook').emit('update dish queue', queue)
+          // })
 
           console.log(type + ' ' + _id + ' connect to ' + restaurantId)
           socket.join(type) // put user into rooms according to their type
-          socket.on('dish cooked', (dish) => {
+          socket.on('dish cooked', dish => {
             // new dish cooked from kitchen, need to send to waiter
             // skip recording to db
             nsp.to('waiter').emit('update dish', dish)
           })
-          socket.on('new request', (request) => {
+          socket.on('new request', request => {
             // new request from customer need to send to waiter
             nsp.to('waiter').emit('update request', request)
           })
-          socket.on('update dish', (dish) => {
+          socket.on('update dish', dish => {
             // dish served or fail send from waiter, server need to update the db
             // update db
             console.log(dish)
@@ -56,7 +56,7 @@ const onConnection = (anonymousClient) => {
             nsp.to('waiter').emit('update dish', dish) // let all other waiter know
             nsp.to('cook').emit('update dish', dish)
           })
-          socket.on('update request', (request) => {
+          socket.on('update request', request => {
             // update db
             console.log(request)
             requests[request._id] = request
@@ -88,7 +88,7 @@ const onConnection = (anonymousClient) => {
 
 io.on('connect', onConnection)
 
-http.listen(PORT, function () {
+http.listen(PORT, function() {
   console.log('Websocket listening at ' + PORT)
 })
 
