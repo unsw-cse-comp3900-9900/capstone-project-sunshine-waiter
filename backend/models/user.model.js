d2b31255c5eea5eee587d314d96e46d608684456
JasonZheng <hbxzknight@gmail.com>
2020-04-13 12:16:40 +1000

    make sure user.password is private

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,81 +1,90 @@
 const mongoose = require('mongoose')
 const { isValidRole } = require('../auth/authorization')
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50,
-  },
-  email: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 10,
-    maxlength: 1023, // **hashed** password
-  },
-  avatar: {
-    type: String, // picture url
-    required: false,
-    minlength: 10,
-    maxlength: 1023,
-  },
-  currentJobs: {
-    type: [
-      {
-        restaurant: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Restaurant',
-          required: true,
-        },
-        role: {
-          type: String,
-          required: true,
-          validate: {
-            validator: isValidRole,
-            message: '{VALUE} is not an valid role.',
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 10,
+      maxlength: 1023, // **hashed** password
+    },
+    avatar: {
+      type: String, // picture url
+      required: false,
+      minlength: 10,
+      maxlength: 1023,
+    },
+    currentJobs: {
+      type: [
+        {
+          restaurant: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Restaurant',
+            required: true,
+          },
+          role: {
+            type: String,
+            required: true,
+            validate: {
+              validator: isValidRole,
+              message: '{VALUE} is not an valid role.',
+            },
           },
         },
-      },
-    ],
-    required: true,
-    default: [],
-  },
-  pendingJobs: {
-    type: [
-      {
-        restaurant: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Restaurant',
-          required: true,
-        },
-        role: {
-          type: String,
-          required: true,
-          validate: {
-            validator: isValidRole,
-            message: '{VALUE} is not an valid role.',
+      ],
+      required: true,
+      default: [],
+    },
+    pendingJobs: {
+      type: [
+        {
+          restaurant: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Restaurant',
+            required: true,
+          },
+          role: {
+            type: String,
+            required: true,
+            validate: {
+              validator: isValidRole,
+              message: '{VALUE} is not an valid role.',
+            },
           },
         },
-      },
-    ],
-    required: true,
-    default: [],
-  },
+      ],
+      required: true,
+      default: [],
+    },
 
-  // website admin; not restaurant
-  isAdmin: {
-    type: Boolean,
-    default: false,
+    // website admin; not restaurant
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-})
+  {
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.password
+      },
+    },
+  }
+)
 
 const User = mongoose.model('User', userSchema)
 
