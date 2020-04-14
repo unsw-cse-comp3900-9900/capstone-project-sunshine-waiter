def466680e96e026c824b64125cbe7cf599cde55
YAOYELU <luyaoyejeren@gmail.com>
2020-04-14 19:55:24 +1000

    rendering staff

---
+++ frontend/src/components/manager/staff/StaffItemCard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import { getUser } from '../../apis/actions/users'
+import { getCookie } from '../../authenticate/Cookies'
+
+class StaffItemCard extends React.Component {
+  state = {
+    staff: null,
+  }
+
+  onSetStaff = data => {
+    this.setState({
+      staff: data,
+    })
+  }
+
+  UNSAFE_componentWillMount = async () => {
+    const { id } = this.props
+    await getUser(getCookie('token'), id, this.onSetStaff)
+  }
+
+  render() {
+    if (this.state.staff === null) {
+      return null
+    }
+    const { name, email, avatar } = this.state.staff
+    return (
+      <div className="item item-box">
+        <img id="avater" src={require('../../homepage/SWLogo.png')} />
+        <div className="middle aligned">
+          <h4>{name}</h4>
+          <small className="email">email: {email}</small>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default StaffItemCard
