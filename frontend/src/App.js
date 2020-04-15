635e8c90d306b961f5feb77519b95e4ddad17f6d
YAOYELU <luyaoyejeren@gmail.com>
2020-04-15 13:49:33 +1000

    finish restaurant polling, this also fixed the problem of restaurant creation did not auto re-render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,10 @@ import Kitchen from './components/Kitchen/Kitchen'
 import Waiter from './components/Waiter/Waiter'
 import Manager from './components/manager/Manager'
 import NotFound from './components/NotFound'
+import { getRestaurants } from './components/apis/actions/restaurants'
+import { getCookie } from './components/authenticate/Cookies'
+import { Polling } from './components/apis/Polling'
+import { compareTwoArrays } from './components/services'
 
 class App extends React.Component {
   //I think reason is when using a tag, it freshes when hitting the page url, so state got freshed back to init
@@ -17,9 +21,21 @@ class App extends React.Component {
   }
 
   updateRestaurants = (restaurants = []) => {
-    this.setState({
-      restaurants: restaurants,
-    })
+    if (
+      !compareTwoArrays(restaurants, this.state.restaurants) ||
+      !compareTwoArrays(this.state.restaurants, restaurants)
+    ) {
+      this.setState({
+        restaurants: restaurants,
+      })
+    }
+  }
+
+  componentDidMount = () => {
+    Polling(
+      () => getRestaurants(getCookie('token'), this.updateRestaurants),
+      1000
+    )
   }
 
   render() {
@@ -28,12 +44,7 @@ class App extends React.Component {
         <Route
           exact
           path="/"
-          children={
-            <Homepage
-              updateRestaurants={this.updateRestaurants}
-              restaurants={this.state.restaurants}
-            />
-          }
+          children={<Homepage restaurants={this.state.restaurants} />}
         />
         <Route
           exact
