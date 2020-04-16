0c8fe8a53d77d479bffafe2ed284d39bb21cc593
YAOYELU <luyaoyejeren@gmail.com>
2020-04-16 18:54:04 +1000

    add unauthorized page

---
+++ frontend/src/components/Unauthorized.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Unauthorized extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Not Authorized</h1>
+        <small>Please contact the manager</small>
+      </div>
+    )
+  }
+}
