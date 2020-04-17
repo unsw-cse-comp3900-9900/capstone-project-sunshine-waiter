02b93e20fd120a01a35e09fe39382adaa92a41cc
Zitong Li <filr0017@gmail.com>
2020-04-17 15:52:49 +1000

    typo fixed

--- frontend/src/components/NavigationButtonCard.js
+++ frontend/src/components/NavigationButtonCard.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const NavigationButtonCard = props => {
+  let className = 'btn btn-primary m-2 '
+  className += props.linkActiveItem === props.linkName ? 'active item' : 'item'
+
   return (
     <Link
       role="button"
       to={props.linkTo}
       name={props.linkName}
       onClick={props.linkOnClick}
-      className="btn btn-primary m-2"
+      className={className}
     >
       <span>{props.children}</span>
       {props.linkName}
