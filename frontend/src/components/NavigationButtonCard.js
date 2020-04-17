175f4836d26d030ae06455c819800a1514efb13a
Zitong Li <filr0017@gmail.com>
2020-04-17 13:30:14 +1000

    restaurant home page UI improved

--- frontend/src/components/NavigationButtonCard.js
+++ frontend/src/components/NavigationButtonCard.js
@@ -3,19 +3,16 @@ import { Link } from 'react-router-dom'
 
 const NavigationButtonCard = props => {
   return (
-    <button className="ui basic button">
-      <Link
-        to={props.linkTo}
-        name={props.linkName}
-        onClick={props.linkOnClick}
-        className={
-          props.linkActiveItem === props.linkName ? 'active item' : 'item'
-        }
-      >
-        <span>{props.children}</span>
-        {props.linkName}
-      </Link>
-    </button>
+    <Link
+      role="button"
+      to={props.linkTo}
+      name={props.linkName}
+      onClick={props.linkOnClick}
+      className="btn btn-primary mx-2"
+    >
+      <span>{props.children}</span>
+      {props.linkName}
+    </Link>
   )
 }
 
--- frontend/src/components/Resturant.jsx
+++ frontend/src/components/Resturant.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 
 import Nivagation from './Navigation'
-import './Resturant.css'
+import './restaurant.css'
 import { getSingleRestaurant } from './apis/actions/restaurants'
 import { getCookie } from './authenticate/Cookies'
 
 class Restaurant extends React.Component {
-  state = {
-    currentRestaurant: {
-      name: '',
-      description: '',
-    },
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentRestaurant: {
+        name: '',
+        description: '',
+      },
+    }
+
+    const { id } = this.props.match.params
+    getSingleRestaurant(getCookie('token'), id, this.onSetState)
   }
 
   onSetState = data => {
@@ -19,26 +26,29 @@ class Restaurant extends React.Component {
     })
   }
 
-  UNSAFE_componentWillMount = () => {
-    const { id } = this.props.match.params
-    getSingleRestaurant(getCookie('token'), id, this.onSetState)
-  }
-
   render() {
     const { id } = this.props.match.params
     const { name, description } = this.state.currentRestaurant
 
     return (
       <div className="restaurant">
-        <img src={require('../resturant.jpg')} alt="" />
-        <div className="welcome-message">
-          <h1 className="ui inverted" style={{ fontSize: '70px' }}>
-            Welcome to the {name}
-          </h1>
-          <small>{description}</small>
-        </div>
-        <div className="buttons">
-          <Nivagation id={id} />
+        <div className="container center">
+          <div className="row">
+            <h1 className="display-1 white text-shadow font-weight-bolder text-shadow">
+              Welcome to
+            </h1>
+          </div>
+          <div className="row">
+            <h1 className="display-4 white text-shadow">{name}</h1>
+          </div>
+          <div className="row">
+            <footer class="blockquote-footer my-3 font-italic">
+              {description}
+            </footer>
+          </div>
+          <div className="row">
+            <Nivagation id={id} />
+          </div>
         </div>
       </div>
     )
