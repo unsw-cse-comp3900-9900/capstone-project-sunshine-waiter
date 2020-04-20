7676b432be2b14d572b304aa186c11f9e5bb36c9
JasonZheng <hbxzknight@gmail.com>
2020-04-20 19:04:53 +1000

    image upload component

---
+++ frontend/src/components/ImageUploader.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import 'antd/dist/antd.css'
+import './index.css'
+import { Upload, message } from 'antd'
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons'
+
+function getBase64(img, callback) {
+  const reader = new FileReader()
+  reader.addEventListener('load', () => callback(reader.result))
+  reader.readAsDataURL(img)
+}
+
+function beforeUpload(file) {
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png'
+  if (!isJpgOrPng) {
+    message.error('You can only upload JPG/PNG file!')
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2
+  if (!isLt2M) {
+    message.error('Image must smaller than 2MB!')
+  }
+  return isJpgOrPng && isLt2M
+}
+
+class ImageUpload extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      url: props.url,
+      headers: props.headers,
+      loading: false,
+    }
+  }
+
+  handleChange = (info) => {
+    if (info.file.status === 'uploading') {
+      this.setState({ loading: true })
+      return
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, (imageUrl) =>
+        this.setState({
+          imageUrl,
+          loading: false,
+        })
+      )
+    }
+  }
+
+  render() {
+    const { url, headers } = this.state
+
+    const uploadButton = (
+      <div>
+        {this.state.loading ? <LoadingOutlined /> : <PlusOutlined />}
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    )
+    const { imageUrl } = this.state
+    return (
+      <Upload
+        name="image"
+        listType="picture-card"
+        className="image-uploader"
+        showUploadList={false}
+        action={this.state.url}
+        beforeUpload={beforeUpload}
+        onChange={this.handleChange}
+        headers={this.state.headers}
+      >
+        {imageUrl ? (
+          <img src={imageUrl} alt="image" style={{ width: '100%' }} />
+        ) : (
+          uploadButton
+        )}
+      </Upload>
+    )
+  }
+}
