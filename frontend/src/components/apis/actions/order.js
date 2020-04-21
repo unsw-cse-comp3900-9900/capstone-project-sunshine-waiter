7d8ff8183849a9d956cdc2a265cf570d86181978
JasonZheng <hbxzknight@gmail.com>
2020-04-22 01:00:34 +1000

    fix request error

--- frontend/src/components/apis/actions/order.js
+++ frontend/src/components/apis/actions/order.js
@@ -1,21 +1,12 @@
 import BaseProvider from '../BaseProvider'
 
-export const createOrder = (
-  // token,
-  restaurantId,
-  param,
-  callback = () => {}
-) => {
-
+export const createOrder = (restaurantId, param, callback = () => {}) => {
   const config = {
     headers: {
-      // 'x-auth-token': token,
-
       'Content-Type': 'application/json',
     },
   }
   const URL = `/restaurants/${restaurantId}/orders/`
-
 
   BaseProvider.post(URL, param, config)
     .then((res) => {
@@ -24,7 +15,6 @@ export const createOrder = (
     .catch((err) => {
       console.log({ err })
     })
-
 }
 
 export const fetchOrderApi = (restaurantId, orderId, callback = () => {}) => {
