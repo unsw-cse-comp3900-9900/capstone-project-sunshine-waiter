ad63dd3e974eeffdfa661a544ce51f9eb3a363f8
JasonZheng <hbxzknight@gmail.com>
2020-04-22 10:28:07 +1000

    remove unnecessary input

--- frontend/src/components/apis/actions/request.js
+++ frontend/src/components/apis/actions/request.js
@@ -1,17 +1,14 @@
 import BaseProvider from '../BaseProvider'
 
-export const sendRequest = (
-  token,
-  restaurantId,
-  param,
-  callback = () => {}
-) => {
+export const sendRequest = (restaurantId, param, callback = () => {}) => {
   const config = {
     headers: {
       'Content-Type': 'application/json',
     },
   }
+
   const URL = `/restaurants/${restaurantId}/request/`
+  console.log({ message: 'jason mark', param, restaurantId, URL })
   BaseProvider.post(URL, param, config)
     .then((res) => {
       callback()
